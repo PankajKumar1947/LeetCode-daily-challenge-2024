8a86a08e26c52c63cbabee38c9c0669a498bc9c9
Pankaj Kumar <pankaj.ky3007@gmail.com>
2024-01-27 22:39:41 +0530

    k inverse pairs array

---
+++ 01_January/27_Jan.cpp
@@ -0,0 +1,23 @@
+//629. K Inverse Pairs Array
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+    int mod=1e9+7;
+    int f(int n, int k, vector<vector<int>>& dp){
+        if(k<=0) return k==0;
+        if(n<=0) return 0;
+        
+        if(dp[n][k]!=-1) return dp[n][k];
+        
+        
+        dp[n][k] = (f(n-1, k, dp) + f(n, k-1, dp))%mod;
+        
+        return dp[n][k] = (dp[n][k] - f(n-1, k-n, dp) + mod)%mod;
+    }
+public:
+    int kInversePairs(int n, int k) {
+        vector<vector<int>> dp(n+1,vector<int>(k+1,-1));
+        return f(n,k,dp);
+    }
+};
